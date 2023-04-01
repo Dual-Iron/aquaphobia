5dba200970232e05a69bc458e559023839efbe25
Dual-Iron <31146412+Dual-Iron@users.noreply.github.com>
2023-04-01 18:01:14 -0400

    Init mod

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -6,9 +6,9 @@ using System.Security.Permissions;
 [assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
 #pragma warning restore CS0618
 
-namespace TestMod;
+namespace Aquaphobia;
 
-[BepInPlugin("com.author.testmod", "Test Mod", "0.1.0")]
+[BepInPlugin("com.dual.aquaphobia", "Aquaphobia", "1.0.0")]
 sealed class Plugin : BaseUnityPlugin
 {
     bool init;
